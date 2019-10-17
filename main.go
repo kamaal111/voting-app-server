618c5c5f70890dca5b4e9c6688c8612f166a7443
Kamaal Farah <kamaal.f1@gmail.com>
2019-10-17 16:35:35 +0200

    Integrating mongodb

--- main.go
+++ main.go
@@ -1,48 +1,107 @@
 package main
 
 import (
+	"context"
 	"encoding/json"
 	"fmt"
 	"net/http"
+	"time"
 
 	"github.com/gorilla/mux"
+	"go.mongodb.org/mongo-driver/bson"
+	"go.mongodb.org/mongo-driver/bson/primitive"
+	"go.mongodb.org/mongo-driver/mongo"
+	"go.mongodb.org/mongo-driver/mongo/options"
 )
 
-const port = "127.0.0.1:8000"
+// DatabaseCollections defines the database collections structure
+type DatabaseCollections struct {
+	sessions string
+}
+
+// Config defines how the configuration structure
+type Config struct {
+	port                string
+	databaseURL         string
+	databaseName        string
+	databaseCollections DatabaseCollections
+}
+
+var config = Config{
+	port:         "127.0.0.1:8000",
+	databaseURL:  "mongodb://127.0.0.1:27017",
+	databaseName: "voting_app",
+	databaseCollections: DatabaseCollections{
+		sessions: "sessions",
+	},
+}
 
 // An Session represents a voting session
 type Session struct {
-	ID   int    `json:"id"`   // Session ID
-	Name string `json:"name"` // Session Name
+	ID   primitive.ObjectID `json:"_id,omitempty" bson:"_id,omitempty"`
+	Name string             `json:"name,omitempty" bson:"name,omitempty"`
 }
 
-func homeLink(writer http.ResponseWriter, request *http.Request) {
+var client *mongo.Client
 
-	fmt.Printf("%s/\n", port)
-	fmt.Fprintf(writer, "Hello and welcome")
+func createSession(response http.ResponseWriter, request *http.Request) {
+	fmt.Printf("POST %s/sessions\n", config.port)
+	response.Header().Add("content-type", "application/json")
+	var session Session
+	json.NewDecoder(request.Body).Decode(&session)
+	collection := client.Database(config.databaseName).Collection(config.databaseCollections.sessions)
+	ctx, cancel := context.WithTimeout(context.Background(), 10*time.Second)
+	defer cancel()
+	result, _ := collection.InsertOne(ctx, session)
+	json.NewEncoder(response).Encode(result)
 }
 
-func getAllSessions(writer http.ResponseWriter, request *http.Request) {
-	// AllSessions represents all available sessions
-	type AllSessions []Session
+func getAllSessions(response http.ResponseWriter, request *http.Request) {
+	fmt.Printf("GET %s/sessions\n", config.port)
+	response.Header().Add("content-type", "application/json")
+	var allSessions []Session
 
-	sessions := AllSessions{
-		{
-			ID:   1,
-			Name: "Kamaal",
-		},
+	collection := client.Database(config.databaseName).Collection(config.databaseCollections.sessions)
+
+	ctx, cancel := context.WithTimeout(context.Background(), 10*time.Second)
+
+	defer cancel()
+
+	cursor, err := collection.Find(ctx, bson.M{})
+
+	if err != nil {
+		response.WriteHeader(http.StatusInternalServerError)
+		response.Write([]byte(`{"message": "` + err.Error() + `"}`))
+		return
 	}
 
-	fmt.Printf("GET %s/sessions\n", port)
-	json.NewEncoder(writer).Encode(sessions)
+	defer cursor.Close(ctx)
+
+	for cursor.Next(ctx) {
+		var session Session
+		cursor.Decode(&session)
+		allSessions = append(allSessions, session)
+	}
+
+	if err := cursor.Err(); err != nil {
+		response.WriteHeader(http.StatusInternalServerError)
+		response.Write([]byte(`{"message": "` + err.Error() + `"}`))
+		return
+	}
+
+	json.NewEncoder(response).Encode(allSessions)
 }
 
 func main() {
+	ctx, cancel := context.WithTimeout(context.Background(), 10*time.Second)
+	defer cancel()
+	clientOptions := options.Client().ApplyURI(config.databaseURL)
+	client, _ = mongo.Connect(ctx, clientOptions)
 	router := mux.NewRouter()
 
-	router.HandleFunc("/", homeLink)
 	router.HandleFunc("/sessions", getAllSessions).Methods("GET")
+	router.HandleFunc("/sessions", createSession).Methods("POST")
 
-	fmt.Printf("Listening on %s\n", port)
-	http.ListenAndServe(port, router)
+	fmt.Printf("Listening on %s\n", config.port)
+	http.ListenAndServe(config.port, router)
 }
