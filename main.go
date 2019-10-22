941228b170f3e06b440ee3dd13f4063751440611
Kamaal Farah <kamaal.f1@gmail.com>
2019-10-22 17:01:18 +0200

    Defining a candidate more specificly

--- main.go
+++ main.go
@@ -50,11 +50,17 @@ var config = Config{
 	},
 }
 
+// Candidate struct represents a candidate
+type Candidate struct {
+	Name  string   `json:"name,omitempty" bson:"name,omitempty"`
+	Votes []string `json:"votes,omitempty" bson:"votes,omitempty"`
+}
+
 // An Session represents a voting session
 type Session struct {
 	ID         primitive.ObjectID `json:"_id,omitempty" bson:"_id,omitempty"`
 	Name       string             `json:"name,omitempty" bson:"name,omitempty"`
-	Candidates []string           `json:"candidates,omitempty" bson:"candidates,omitempty"`
+	Candidates []Candidate        `json:"candidates,omitempty" bson:"candidates,omitempty"`
 }
 
 var client *mongo.Client
